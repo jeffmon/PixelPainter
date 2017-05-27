07f55b6099aa487b5d7e839fa6ba8002050b0d6e
jeffmon <jeffmon@gmail.com>
2017-05-27 12:38:26 -1000

    set divs and css

---
+++ js/PixelPainter.js
@@ -0,0 +1,18 @@
+var createToolBox = document.createElement("div");
+createToolBox.id = "toolBox";
+pixelPainter.appendChild(createToolBox);
+
+var createPPCanvas = document.createElement("div");
+createPPCanvas.id = "pp-canvas";
+pixelPainter.appendChild(createPPCanvas);
+
+
+
+var createColorPalette = document.createElement("div");
+createColorPalette.id = "colorPalette";
+toolBox.appendChild(createColorPalette);
+
+var createButtons = document.createElement("div");
+createButtons.id = "buttons";
+toolBox.appendChild(createButtons);
+
