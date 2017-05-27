4e1726b8716098f6ab096369b803afaedb6b0102
James Moore <jameslmoore@gmail.com>
2017-05-27 13:39:44 -1000

    color chooser working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -12,7 +12,46 @@ var createColorPalette = document.createElement("div");
 createColorPalette.id = "colorPalette";
 toolBox.appendChild(createColorPalette);
 
-var createButtons = document.createElement("div");
-createButtons.id = "buttons";
-toolBox.appendChild(createButtons);
+var createOptions = document.createElement("div");
+createOptions.id = "options";
+toolBox.appendChild(createOptions);
 
+
+
+var lastColorPicked = null;
+
+var colorArray = ['red', 'blue', 'black'];
+
+function addColor(){
+  lastColorPicked = this.style.backgroundColor;
+  console.log(lastColorPicked);
+}
+
+(function(){
+  for (var i = 0; i < colorArray.length; i++){
+    var createColorChoice = document.createElement("div");
+    createColorChoice.className = "colorChoice";
+    var colorHelper = colorArray[i];
+    console.log(colorHelper);
+    createColorChoice.style.backgroundColor = colorArray[i];
+    createColorChoice.addEventListener("click", addColor);
+    colorPalette.appendChild(createColorChoice);
+  }
+})();
+
+
+
+// var createColorChoice = document.createElement("div");
+// createColorChoice.className = "colorChoice";
+// createColorChoice.style.backgroundColor = "red";
+// colorPalette.appendChild(createColorChoice);
+
+// var createColorChoice = document.createElement("div");
+// createColorChoice.className = "colorChoice";
+// createColorChoice.style.backgroundColor = "blue";
+// colorPalette.appendChild(createColorChoice);
+
+// var createColorChoice = document.createElement("div");
+// createColorChoice.className = "colorChoice";
+// createColorChoice.style.backgroundColor = "black";
+// colorPalette.appendChild(createColorChoice);
